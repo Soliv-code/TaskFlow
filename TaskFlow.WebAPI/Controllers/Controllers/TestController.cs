a36791d1928372868fcb1e157734f0ec2f3586cf
Илья <t3mp1er@mail.ru>
2026-09-07 09:11:58 +0300

    feat: add Refresh Token rotation, global exception handling, native logging and authorize test

---
+++ TaskFlow.WebAPI/Controllers/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskFlow.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize] // <-- Этот атрибут требует валидный JWT-токен в заголовке
+public class TestController : ControllerBase
+{
+    [HttpGet("secure-data")]
+    public IActionResult GetSecureData()
+    {
+        return Ok(new { message = "Доступ разрешен! Вы успешно аутентифицированы." });
+    }
+}
