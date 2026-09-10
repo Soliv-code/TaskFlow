e041afd202e5ab6ad3ccdf6d4956d6739502a276
Илья <t3mp1er@mail.ru>
2026-09-10 05:23:09 +0300

    Repeat

---
+++ TaskFlow.Domain/Entities/Project.cs
@@ -0,0 +1,20 @@
+﻿namespace TaskFlow.Domain.Entities;
+
+public partial class Project
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public int OwnerId { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public virtual User Owner { get; set; } = null!;
+
+    public virtual ICollection<Task> Tasks { get; set; } = new List<Task>();
+}
---
+++ TaskFlow.Domain/Entities/TaskStatus.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskFlow.Domain.Entities;
+
+public partial class TaskStatus
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public int SortOrder { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual ICollection<Task> Tasks { get; set; } = new List<Task>();
+}
